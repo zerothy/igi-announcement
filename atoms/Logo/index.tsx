ad69dcbe8af50506ed042c4540d8f7a26af65c73
zerothy <zxuyagaming@gmail.com>
2024-07-21 23:22:45 +0700

    fix: overflow

--- atoms/Logo/index.tsx
+++ atoms/Logo/index.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
     return (
         <div>
             <h1 
-                className="font-lexend z-[90] text-5xl pb-10 font-semibold text-[#FFEDFD]" 
+                className="font-lexend relative z-[90] text-3xl md:text-5xl pb-4 md:pb-10 font-semibold text-[#FFEDFD]" 
                 style={{
                     textShadow: '0px 4px 3px #A61E24',
                 }}
--- components/GamePage/index.tsx
+++ components/GamePage/index.tsx
@@ -1,17 +1,11 @@
 import Logo from '@/atoms/Logo'
 
-import LeftGroup from '@/molecules/LeftGroup'
-import RightGroup from '@/molecules/RightGroup'
-
 import Board from '@/organisms/Board'
 
 export default function GamePage() {
     return (
         <div className="flex flex-col w-screen h-screen justify-center items-center bg-[#00090A]">
-            <div className='absolute w-screen h-full z-0 flex justify-between'>
-                <LeftGroup />
-                <RightGroup />
-            </div>
+            <img src="/images/igi_bg.png" alt="IGI BG" className='z-0 absolute object-cover w-screen h-screen' />
             <Logo />
             <Board />
         </div>

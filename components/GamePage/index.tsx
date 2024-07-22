68a1395fa8c54725bbd320fa4ea78a680164c494
zerothy <zxuyagaming@gmail.com>
2024-07-23 00:29:55 +0700

    finalize

--- components/GamePage/index.tsx
+++ components/GamePage/index.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 import Logo from '@/atoms/Logo'
 import Background from '@/atoms/Background'
 import Wins from '@/atoms/Wins'
-import End from '@/molecules/End'
+import End from '@/atoms/End'
 
 import Widgets from '@/organisms/Widgets'
 import Board from '@/organisms/Board'
@@ -37,7 +37,9 @@ export default function GamePage() {
             <Background />
             <Logo title={`${totalWins === 0 ? 'SOLVE TO REVEAL IGI GAME JAM 2ND RUNNER UP' : totalWins === 1 ? 'SOLVE TO REVEAL IGI GAME JAM 1ST RUNNER UP' : 'SOLVE TO REVEAL IGI GAME JAM CHAMPION'}`} size={4} />
             <div className='flex flex-col lg:flex-row'>
-                <div className='lg:w-[16rem] lg:mr-6' />
+                <div className='lg:w-[16rem] lg:mr-6'>
+                    <h1 className="text-xl md:text-2xl bg-[rgba(69,10,10,0.8)] font-urbanist font-semibold text-[#FFEDFD] opacity-80 text-center">Click on the tile you wish to move!</h1>
+                </div>
                 <Board />
                 <Widgets timeLeft={timeLeft} />
             </div>
